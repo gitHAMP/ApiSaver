0010c63100e4b4036bbf5ad327e630d8f0cd14f7
Henry Antonio Mendoza Puerta <hmendo81@gmail.com>
2021-06-20 12:34:50 -0500

    frontend

--- src/main/java/com/example/controllers/OutcomeController.java
+++ src/main/java/com/example/controllers/OutcomeController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "https://saver-upao.netlify.app/")
+@CrossOrigin(origins = "https://saver-upao.netlify.app")
 @Api
 @RestController
 @RequestMapping("/outcomes")
